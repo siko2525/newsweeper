06205746920a231af700aa4013ecfd8cd6c3c50d
siko2525 <s1f102301820@iniad.org>
2024-05-21 02:53:53 +0900

    爆弾周りの数字表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import styles from './index.module.css';
-import { stringifyCookie } from 'next/dist/compiled/@edge-runtime/cookies';
-import { Console } from 'console';
 
 const Home = () => {
   // 0 -> 未クリック
   // 1 -> 左クリック
   // 2 -> はてな
   // 3 -> 旗
-  const [board, setBoard] = useState<(0 | 1 | 2 | 3)[][]>([
+  const [userInput, setUserInput] = useState<(0 | 1 | 2 | 3)[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -32,7 +30,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const minesweeper: number[][] = [
+  const board: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,53 +48,89 @@ const Home = () => {
     [0, 1],
     [1, 1],
     [1, 0],
-    [1, -1],
+    [-1, -1],
     [0, -1],
     [1, -1],
   ];
 
-  const isPlaying = board.some((row) => row.some((input) => input !== 0));
-  const isFailure = board.some((row, y) =>
+  const isPlaying = userInput.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInput.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1),
   );
   //bombを作る
-  const bombCreate = (bombmap: number[][], x: number, y: number) => {
+  const bombCreate = (bombMap: number[][], x: number, y: number) => {
     console.log(2);
     let bombCount = 0;
-    const newBombMap = structuredClone(bombmap);
+    const newBombMap = structuredClone(bombMap);
     while (bombCount < 10) {
       const bombX = Math.floor(Math.random() * 9);
       const bombY = Math.floor(Math.random() * 9);
-      console.log(bombX, bombY);
       if (newBombMap[bombY][bombX] === 1 || (bombX === x && bombY === y)) continue;
       newBombMap[bombY][bombX] = 1;
       bombCount++;
     }
-
-    console.log('pao-n', newBombMap);
     return newBombMap;
   };
 
   //クリックしたときの挙動
   const onClick = (x: number, y: number) => {
-    console.log(1);
+    // const isExistBomb = board.some((row) => row.some((input) => input === 1));
+    const isExistBomb = userInput.flat().some((input) => input === 1);
+    //flatで二次元配列を一次元にもってきたので、someを一個にできた
     const newBombMap = structuredClone(bombMap);
-    board[y][x] = 1;
-    const newnewBombMap = bombCreate(newBombMap, x, y);
-    setBombMap(newnewBombMap);
+    const newUserInput = structuredClone(userInput);
+    newUserInput[y][x] = 1;
+    if (isExistBomb !== true) {
+      const newNewBombMap = bombCreate(newBombMap, x, y);
+      setBombMap(newNewBombMap);
+    }
+    setUserInput(newUserInput);
   };
 
+  const fusion = (x: number, y: number, newUserInput: number[][], newBombMap: number[][]) => {
+    if (newBombMap[y][x] === 1 && newUserInput[y][x] === 1) {
+      board[y][x] = 11;
+      console.log(3);
+      for (let x = 0; x < 9; x++) {
+        for (let y = 0; y < 9; y++) {
+          if (newBombMap[y][x] === 1 && newUserInput[y][x] === 0) {
+            board[y][x] = 11;
+          }
+        }
+      }
+      return;
+    }
+    if (newUserInput[y][x] === 1) {
+      let charge = 0;
+      for (const dir of directions) {
+        if (
+          newBombMap[y + dir[0]] !== undefined &&
+          newBombMap[x + dir[1]] !== undefined &&
+          newBombMap[y + dir[0]][x + dir[1]] === 1
+        ) {
+          charge++;
+        }
+      }
+      board[y][x] = charge;
+    }
+  };
+
+  for (let x = 0; x < 9; x++) {
+    for (let y = 0; y < 9; y++) {
+      fusion(x, y, userInput, bombMap);
+    }
+  }
   return (
     <div className={styles.container}>
       <div className={styles.frame}>
-        <div className={styles.smilearea}>
-          <div className={styles.bombcount} />
+        <div className={styles.smileArea}>
+          <div className={styles.bombCount} />
           <div className={styles.smile} />
           <div className={styles.timer} />
         </div>
 
         <div className={styles.board}>
-          {bombMap.map((row, y) =>
+          {board.map((row, y) =>
             row.map((color, x) => (
               <div
                 className={styles.bomb}
@@ -104,7 +138,6 @@ const Home = () => {
                 onClick={() => onClick(x, y)}
                 style={{ backgroundPosition: color * -30 + 30 }}
               >
-                {bombMap[y][x]}
                 {color === -1 && <div className={styles.stone} />}
               </div>
             )),
